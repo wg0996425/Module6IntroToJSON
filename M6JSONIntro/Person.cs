dc96822c3dbe894e2be38a9605dbd396b0620b94
wg0996425 <wg0996425@otc.edu>
2025-02-27 09:41:57 -0600

    Initial Commit

---
+++ M6JSONIntro/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6JSONIntro
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool IsStudent { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name - {Name}, Age - {Age}, Email - {Email}, Student? - {IsStudent}";
+        }
+    }
+}
---
+++ M6JSONIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using M6JSONIntro;
+using Newtonsoft.Json;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            List<Person> people = [];
+
+            Person person1 = new()
+            {
+                Name = "John Doe",
+                Age = 30,
+                Email = "johndoe1234@companya.com",
+                IsStudent = true
+            };
+            people.Add(person1);
+
+            Person person2 = new Person
+            {
+                Name = "Joe Loser",
+                Age = 22,
+                Email = "joeloser4321@companyb.com",
+                IsStudent = true
+            };
+            people.Add(person2);
+
+            // SERIALIZE LIST
+            string json = JsonConvert.SerializeObject(people);
+            Console.WriteLine("Serialized JSON: " + json);
+
+            // DESERIALIZE LIST
+            var deserializedPeople = JsonConvert.DeserializeObject<List<Person>>(json);
+            Console.WriteLine($"Deserialized JSON: \n{String.Join("\n", deserializedPeople)}");
+        }
+        catch (JsonReaderException jre)
+        {
+            Console.WriteLine($"Invalid JSON Error: {jre}");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"No JSON File Found!");
+        }
+    }
+}
